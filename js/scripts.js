60d34a3d078bff350f03dd9a8d8c6b73eee44f07
mulimuoki001 <b.muoki@alustudent.com>
2024-07-23 03:24:22 +0300

    settings page

--- js/scripts.js
+++ js/scripts.js
@@ -311,12 +311,35 @@ areaChart.render();
 
   const themeSwitch = document.getElementById('themeSwitch');
 
+// Get the current theme from local storage
+const currentTheme = localStorage.getItem('theme');
+
+// Set the initial theme based on the stored value or default to light theme
+if (currentTheme === 'dark') {
+  themeSwitch.checked = true;
+  document.body.classList.add('dark-theme');
+}
+
+// Add event listener to the switch slider
 themeSwitch.addEventListener('change', function() {
-    if(this.checked) {
-        document.body.classList.add('dark-theme');
-        document.querySelector('.sidebar').classList.add('dark-theme');
-    } else {
-        document.body.classList.remove('dark-theme');
-        document.querySelector('.sidebar').classList.remove('dark-theme');
+  const theme = this.checked ? 'dark' : 'light';
+
+  // Store the selected theme in local storage
+  localStorage.setItem('theme', theme);
+
+  // Apply the theme to all pages
+  document.body.classList.toggle('dark-theme', theme === 'dark');
+});
+const profilePictureInput = document.getElementById('profilePicture');
+const profileImage = document.getElementById('profileImage');
+
+profilePictureInput.addEventListener('change', function() {
+    const file = this.files[0];
+    const reader = new FileReader();
+
+    reader.onload = function(e) {
+        profileImage.src = e.target.result;
     }
+
+    reader.readAsDataURL(file);
 });
